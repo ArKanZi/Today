56674255473b65f29a8ed9e823365e557a6e58dd
ArKanZi <ayushku.ak@gmail.com>
2025-12-26 11:14:40 +0530

    many changes beta 0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "beta-0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
